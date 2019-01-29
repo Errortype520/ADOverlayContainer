2c83fac6b5666f1dacfe76a04bf18c0aa1178e1f
Joe Burgess <errortype520@gmail.com>
2019-01-29 09:41:17 -0500

    carthage

---
+++ OverlayContainerFramework/OverlayContainerFramework.h
@@ -0,0 +1,19 @@
+//
+//  OverlayContainerFramework.h
+//  OverlayContainerFramework
+//
+//  Created by Joe Burgess on 1/29/19.
+//  Copyright © 2019 Gaétan Zanella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OverlayContainerFramework.
+FOUNDATION_EXPORT double OverlayContainerFrameworkVersionNumber;
+
+//! Project version string for OverlayContainerFramework.
+FOUNDATION_EXPORT const unsigned char OverlayContainerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OverlayContainerFramework/PublicHeader.h>
+
+
