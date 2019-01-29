11c589e283f11da7ce4e68ae3ecaccc5cd520538
Joe Burgess <errortype520@gmail.com>
2019-01-29 12:39:36 -0500

    Carthage update

---
+++ ADOverlayContainer/ADOverlayContainer/ADOverlayContainer.h
@@ -0,0 +1,29 @@
+//
+//  ADOverlayContainer.h
+//  ADOverlayContainer
+//
+//  Created by Joe Burgess on 1/29/19.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ADOverlayContainer.
+FOUNDATION_EXPORT double ADOverlayContainerVersionNumber;
+
+//! Project version string for ADOverlayContainer.
+FOUNDATION_EXPORT const unsigned char ADOverlayContainerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ADOverlayContainer/PublicHeader.h>
+
+#import "OverlayTranslationParameters.h"
+#import "RushingForwardTargetNotchPolicy.h"
+#import "OverlayScrollViewDelegate.h"
+#import "OverlayContainerViewController.h"
+#import "SpringOverlayTranslationAnimationController.h"
+#import "RubberBandOverlayTranslationFunction.h"
+#import "OverlayContainerContextTargetNotchPolicy.h"
+#import "OverlayContainerContextTransitioning.h"
+#import "OverlayTransitioningDelegate.h"
+#import "OverlayTranslationTargetNotchPolicy.h"
+#import "OverlayAnimatedTransitioning.h"
+#import "OverlayContainerViewControllerDelegate.h"
